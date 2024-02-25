5941ed16b90190a3d0e15bb1f58dbcf0a5330c8d
LuisaP28 <respaldoluisap@gmial.com>
2024-02-24 20:16:38 -0500

    Se crean los selectores para register.js

---
+++ js/register.js
@@ -0,0 +1,10 @@
+//Selectores
+const formRegister = document.getElementById("form");
+const nameUser = document.getElementById("nameUser");
+const emailUser = document.getElementById("email");
+const adressUser = document.getElementById("adress");
+const passwordUser = document.getElementById("password");
+const confirmPasswordUser = document.getElementById("confirmPassword");
+
+
+
