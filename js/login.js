d52b9eb5686123a8a5e9c9a421671112c7a6fb15
Valeria <valehm248@gmail.com>
2024-02-24 20:29:55 -0500

    primer evento en login

---
+++ js/login.js
@@ -0,0 +1,18 @@
+//Selectores
+
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmPassword");
+//Eventos
+
+form.addEventListener("submit", (event) =>{
+    event.preventDefault();
+
+    logIn()
+});
+
+async function logIn(){
+    cons
+}
+
