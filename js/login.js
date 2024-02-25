023113a6500551b057f3b1ff8abf4a5afd3e5727
Valeria <valehm248@gmail.com>
2024-02-24 21:42:06 -0500

    login terminado

--- js/login.js
+++ js/login.js
@@ -4,6 +4,7 @@ const form = document.getElementById("form");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const confirmPassword = document.getElementById("confirmPassword");
+const URL_BASE = "http://localhost:3000/users"
 //Eventos
 
 form.addEventListener("submit", (event) =>{
@@ -13,6 +14,20 @@ form.addEventListener("submit", (event) =>{
 });
 
 async function logIn(){
-    cons
+    const response = await fetch (`${URL_BASE}?email=${email.value}`);
+    const data = await response.json();
+
+    if(!data){
+        console.error("Email no registrado")
+        return;
+    }
+
+    if(data[0].password !== password.value){
+        console.error("contraseña inválida");
+        return;
+    }
+
+    window.location.href = "inicio.html"
+
 }
 
